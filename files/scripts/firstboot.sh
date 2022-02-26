70583786ae9188d172ee37aa2e63abb69fef8878
pyavitz <pyavitz@gmail.com>
2022-02-26 12:35:43 -0500

    `firstboot`
    ... rewrite

--- files/scripts/firstboot.sh
+++ files/scripts/firstboot.sh
@@ -2,117 +2,28 @@
 
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-### Functions
-grow_mmcblk(){
-bash growpart /dev/mmcblk0 2 > /dev/null 2>&1
-sleep 1s
-if [[ `blkid | grep ext4` ]]; then
-	resize2fs /dev/mmcblk0p2 > /dev/null 2>&1;
-else
-	if [[ `blkid | grep btrfs` ]]; then
-		btrfs filesystem resize max / > /dev/null 2>&1;
-	else
-		if [[ `blkid | grep xfs` ]]; then
-		xfs_growfs -d / > /dev/null 2>&1;
-		fi
-	fi
-fi
-}
+BOOT=`findmnt -v -n -o SOURCE /boot`
+ROOTFS=`findmnt -v -n -o SOURCE /`
+GROW=`findmnt -v -n -o SOURCE / | sed 's/p/ /'`
+GROW_SD=`findmnt -v -n -o SOURCE / | sed 's/./& /8'`
 
-grow_mmcblk1(){
-bash growpart /dev/mmcblk1 2 > /dev/null 2>&1
-sleep 1s
-if [[ `blkid | grep ext4` ]]; then
-	resize2fs /dev/mmcblk1p2 > /dev/null 2>&1;
-else
-	if [[ `blkid | grep btrfs` ]]; then
-		btrfs filesystem resize max / > /dev/null 2>&1;
-	else
-		if [[ `blkid | grep xfs` ]]; then
-			xfs_growfs -d / > /dev/null 2>&1;
-		fi
-	fi
-fi
-}
-
-grow_sda(){
-bash growpart /dev/sda 2 > /dev/null 2>&1
-sleep 1s
-if [[ `blkid | grep ext4` ]]; then
-	resize2fs /dev/sda2 > /dev/null 2>&1;
-else
-	if [[ `blkid | grep btrfs` ]]; then
-		btrfs filesystem resize max / > /dev/null 2>&1;
-	else
-		if [[ `blkid | grep xfs` ]]; then
-			xfs_growfs -d / > /dev/null 2>&1;
-		fi
-	fi
-fi
-}
-
-grow_nvme(){
-bash growpart /dev/nvme0n1 2 > /dev/null 2>&1
-sleep 1s
-if [[ `blkid | grep ext4` ]]; then
-	resize2fs /dev/nvme0n1p2 > /dev/null 2>&1;
-else
-	if [[ `blkid | grep btrfs` ]]; then
-		btrfs filesystem resize max / > /dev/null 2>&1;
-	else
-		if [[ `blkid | grep xfs` ]]; then
-		xfs_growfs -d / > /dev/null 2>&1;
-		fi
-	fi
-fi
-}
-
-chk_mmcblk(){
-bash fsck.fat -trawl /dev/mmcblk0p1 > /dev/null 2>&1
-}
-
-chk_mmcblk1(){
-bash fsck.fat -trawl /dev/mmcblk1p1 > /dev/null 2>&1
-}
-
-chk_sda(){
-bash fsck.fat -trawl /dev/sda1 > /dev/null 2>&1
-}
-
-chk_nvme(){
-bash fsck.fat -trawl /dev/nvme0n1p1 > /dev/null 2>&1
-}
-
+# Functions
 partition_uuid(){
 echo 'ROOT_PARTUUID="' > root1
-if [ -e /dev/mmcblk0p2 ]; then
-	blkid -o export -- "/dev/mmcblk0p2" | sed -ne 's/^PARTUUID=//p' > root2;
-else
-	if [ -e /dev/mmcblk1p2 ]; then
-		blkid -o export -- "/dev/mmcblk1p2" | sed -ne 's/^PARTUUID=//p' > root2;
-	else
-		if [ -e /dev/sda2 ]; then
-			blkid -o export -- "/dev/sda2" | sed -ne 's/^PARTUUID=//p' > root2;
-		else
-			if [ -e /dev/nvme0n1p2 ]; then
-				blkid -o export -- "/dev/nvme0n1p2" | sed -ne 's/^PARTUUID=//p' > root2;
-			fi
-		fi
-	fi
-fi
+blkid -o export -- $ROOTFS | sed -ne 's/^PARTUUID=//p' > root2
 echo '"' > root3
 paste -d '\0' root1 root2 root3  > /etc/opt/root-pid.txt
 rm -f root1 root2 root3
 }
 
 create_cmdline(){
-if [[ `blkid | grep ext4` ]]; then
+if [[ `blkid | grep -w "ext4"` ]]; then
 	cmdline_ext4 > /dev/null 2>&1;
 else
-	if [[ `blkid | grep btrfs` ]]; then
+	if [[ `blkid | grep -w "btrfs"` ]]; then
 		cmdline_btrfs > /dev/null 2>&1;
 	else
-		if [[ `blkid | grep xfs` ]]; then
+		if [[ `blkid | grep -w "xfs"` ]]; then
 			cmdline_xfs > /dev/null 2>&1;
 		fi
 	fi
@@ -146,76 +57,62 @@ EOF
 rm -f /etc/opt/root-pid.txt
 }
 
-fix_cmdline(){
-if [[ `grep -w "arm" "/etc/opt/soc.txt"` ]]; then
-	partition_uuid;
-	sleep 1s
-	create_cmdline;
-fi
-}
-
 disable_bthelper(){
 if [[ `dmesg | grep -w "Raspberry\ Pi\ 2"` ]]; then
 	update-rc.d -f bthelper remove;
-else
-	if [[ `dmesg | grep -w "Raspberry\ Pi\ Model\ B\ Rev\ 1"` ]]; then
-		update-rc.d -f bthelper remove;
-	else
-		if [[ `dmesg | grep -w "Raspberry\ Pi\ Model\ B\ Rev\ 2"` ]]; then
-			update-rc.d -f bthelper remove;
-		fi
-	fi
+fi
+if [[ `dmesg | grep -w "Raspberry\ Pi\ Model\ B\ Rev\ 1"` ]]; then
+	update-rc.d -f bthelper remove;
+fi
+if [[ `dmesg | grep -w "Raspberry\ Pi\ Model\ B\ Rev\ 2"` ]]; then
+	update-rc.d -f bthelper remove;
 fi
 }
 
-### Grow Partition
+# Expand root filesystem
 if [[ `grep -w "Devuan" "/etc/os-release"` ]]; then
-	echo
+	echo ""
 	echo -e " \e[0;31mExpanding root filesystem\e[0m ...";
 fi
-if [ -e /dev/mmcblk0 ]; then
-	grow_mmcblk;
+if [[ `findmnt -v -n -o SOURCE / | grep "mmc"` ]]; then
+	bash growpart $GROW > /dev/null 2>&1;
+fi
+if [[ `findmnt -v -n -o SOURCE / | grep "nvme"` ]]; then
+	bash growpart $GROW > /dev/null 2>&1;
+fi
+if [[ `findmnt -v -n -o SOURCE / | grep "sd"` ]]; then
+	bash growpart $GROW_SD > /dev/null 2>&1;
+fi
+sleep 1s
+if [[ `blkid | grep -w "ext4"` ]]; then
+	resize2fs $ROOTFS > /dev/null 2>&1;
 else
-	if [ -e /dev/mmcblk1 ]; then
-		grow_mmcblk1;
+	if [[ `blkid | grep -w "btrfs"` ]]; then
+		btrfs filesystem resize max / > /dev/null 2>&1;
 	else
-		if [ -e /dev/sda ]; then
-			grow_sda;
-		else
-			if [ -e /dev/nvme0 ]; then
-				grow_nvme;
-			fi
+		if [[ `blkid | grep -w "xfs"` ]]; then
+			xfs_growfs -d / > /dev/null 2>&1;
 		fi
 	fi
 fi
 
-### Fix boot partition
+# Fsck boot partition
 if [[ `grep -w "Devuan" "/etc/os-release"` ]]; then
 	echo -e " \e[0;31mRunning fsck on boot partition\e[0m ...";
 fi
 umount /boot
 sleep 1s
-if [ -e /dev/mmcblk0 ]; then
-	chk_mmcblk;
-else
-	if [ -e /dev/mmcblk1 ]; then
-		chk_mmcblk1;
-	else
-		if [ -e /dev/sda ]; then
-			chk_sda;
-		else
-			if [ -e /dev/nvme0 ]; then
-				chk_nvme;
-			fi
-		fi
-	fi
-fi
+bash fsck.fat -trawl $BOOT > /dev/null 2>&1
 sleep 1s
 mount /boot
 sleep 1s
-fix_cmdline
+if [[ `grep -w "arm" "/etc/opt/soc.txt"` ]]; then
+	partition_uuid;
+	sleep 1s;
+	create_cmdline;
+fi
 
-### Clean up
+# Clean up
 if [[ `grep -w "Debian" "/etc/os-release"` ]]; then
 	rm -f /var/cache/debconf/*
 	rm -f /usr/local/sbin/firstboot
@@ -236,4 +133,4 @@ else
 	fi
 fi
 
-exit
+exit 0
