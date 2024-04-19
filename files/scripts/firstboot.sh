8928a235f8b6c9a17f73084739e21866b9628874
Patrick Yavitz <pyavitz@xxxxx.com>
2024-04-19 04:03:32 -0400

    files: scripts: firstboot.sh: `maintenance`
    Signed-off-by: Patrick Yavitz <pyavitz@xxxxx.com>

--- files/scripts/firstboot.sh
+++ files/scripts/firstboot.sh
@@ -2,33 +2,13 @@
 
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-if [[ -f "/etc/opt/board.txt" ]]; then
-	. /etc/opt/board.txt
-fi
+if [[ -f "/etc/opt/board.txt" ]]; then . /etc/opt/board.txt; fi
 
 DISTRO=`cat /etc/os-release | grep -w NAME | sed 's/NAME=//g' | sed 's/"//g' | sed 's/ GNU\/Linux//g'`
 BOOT=`findmnt -v -n -o SOURCE /boot/broadcom`
 ROOTFS=`findmnt -v -n -o SOURCE /`
 GROW=`findmnt -v -n -o SOURCE / | sed 's/p/ /'`
 GROW_SD=`findmnt -v -n -o SOURCE / | sed 's/./& /8'`
-
-# functions
-create_cmdline(){
-ROOT_PARTUUID=`blkid -o export -- $ROOTFS | sed -ne 's/^PARTUUID=//p'`
-if [[ `findmnt -v -n -o FSTYPE / | grep -w "ext4"` ]]; then
-	ROOTFSTYPE="rootfstype=ext4"
-fi
-if [[ `findmnt -v -n -o FSTYPE / | grep -w "btrfs"` ]]; then
-	ROOTFSTYPE="rootfstype=btrfs rootflags=subvol=@"
-fi
-if [[ `findmnt -v -n -o FSTYPE / | grep -w "xfs"` ]]; then
-	ROOTFSTYPE="rootfstype=xfs"
-fi
-rm -f /boot/broadcom/cmdline.txt
-tee /boot/broadcom/cmdline.txt <<EOF
-console=serial0,115200 console=tty1 root=PARTUUID=${ROOT_PARTUUID} ${ROOTFSTYPE} fsck.repair=yes loglevel=5 net.ifnames=0 firmware_class.path=/lib/firmware/updates/brcm rootwait
-EOF
-}
 
 disable_bthelper(){
 RPI_REV=`cat /proc/cpuinfo | grep Raspberry | sed 's/[^ ]* //' | sed 's/://g' | sed -e 's/^[ \t]*//'`
@@ -41,7 +21,7 @@ fi
 # expand root filesystem
 if [[ "$DISTRO" == "Devuan" ]]; then
 	echo ""
-	echo -e " \033[1mExpanding root filesystem\033[0m ...";
+	echo -e " \033[1mExpanding root filesystem\033[0m ..."
 fi
 if [[ `findmnt -v -n -o SOURCE / | grep "mmc"` ]] || [[ `findmnt -v -n -o SOURCE / | grep "nvme"` ]]; then
 	bash growpart $GROW > /dev/null 2>&1
@@ -60,32 +40,36 @@ if [[ `findmnt -v -n -o FSTYPE / | grep -w "xfs"` ]]; then
 	xfs_growfs -d / > /dev/null 2>&1
 fi
 
-# fsck boot partition
+# boot partition
 if [[ -d "/boot/broadcom" ]]; then
-	if [[ "$DISTRO" == "Devuan" ]]; then
-		echo -e " \033[1mRunning fsck on boot partition\033[0m ...";
-	fi
+	if [[ "$DISTRO" == "Devuan" ]]; then echo -e " \033[1mRunning fsck on boot partition\033[0m ..."; fi
 	umount /boot/broadcom
 	sleep .75
 	bash fsck.fat -trawl $BOOT > /dev/null 2>&1
 	sleep .75
 	mount /boot/broadcom
 	sleep .75
-	if [[ "$ARCH" == "arm" ]]; then
-		create_cmdline
-	fi
 fi
+if [[ "$ARCH" == "arm" ]]; then
+	ROOT_PARTUUID=`blkid -o export -- $ROOTFS | sed -ne 's/^PARTUUID=//p'`
+	if [[ `findmnt -v -n -o FSTYPE / | grep -w "ext4"` ]]; then ROOTFSTYPE="rootfstype=ext4"; fi
+	if [[ `findmnt -v -n -o FSTYPE / | grep -w "btrfs"` ]]; then ROOTFSTYPE="rootfstype=btrfs rootflags=subvol=@"; fi
+	if [[ `findmnt -v -n -o FSTYPE / | grep -w "xfs"` ]]; then ROOTFSTYPE="rootfstype=xfs"; fi
+	rm -f /boot/broadcom/cmdline.txt
+	tee /boot/broadcom/cmdline.txt <<EOF
+console=serial0,115200 console=tty1 root=PARTUUID=${ROOT_PARTUUID} ${ROOTFSTYPE} fsck.repair=yes loglevel=5 net.ifnames=0 firmware_class.path=/lib/firmware/updates/brcm rootwait
+EOF
+fi
+
 
 # finish
 if [[ "$DISTRO" == "Devuan" ]]; then
 	disable_bthelper
 	update-rc.d firstboot remove
 	rm -f /etc/init.d/firstboot
-	rm -f /var/cache/debconf/*
-	rm -f /usr/local/sbin/firstboot
+	rm -fdr /var/cache/debconf/*
 else
-	rm -f /var/cache/debconf/*
-	rm -f /usr/local/sbin/firstboot
+	rm -fdr /var/cache/debconf/*
 	systemctl disable firstboot
 fi
 
